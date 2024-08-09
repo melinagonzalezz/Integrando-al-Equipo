33c117a45aa7cd30d993c07cd7adb3e782abeec3
Mel3343 <meligonzalezgomez12@gmail.com>
2024-08-09 13:27:03 -0300

    Add container to html and css. Change details on Javascript

--- scripts.js
+++ scripts.js
@@ -1,15 +1,33 @@
+//Cargar DOM
+
+document.addEventListener('DOMContentLoaded',function() {
+  
   // Mostrar un mensaje de bienvenida en la primera carga de la página
-  if (window.location.pathname.endsWith('page1.html')) {
+  if (window.location.pathname.endsWith('index.html')) {
     window.onload = function() {
         alert("¡Bienvenido a nuestra Galería de Artistas!");
     };
 }
 
-// Añadir eventos a los botones para mostrar alertas cuando se haga clic
-document.getElementById("prevPage").addEventListener("click", function(event) {
+// Añadir eventos a los botones para mostrar alertas cuando se haga click
+
+var prevPageButton = document.getElementById("prevPage");
+var nextPageButton = document.getElementById("nextPage");
+
+
+if (prevPageButton) {
+prevPageButton.addEventListener("click", function(event) {
     alert("Estás regresando al artista anterior.");
 });
 
-document.getElementById("nextPage").addEventListener("click", function(event) {
+}
+
+
+if (nextPageButton) {
+nextPageButton.addEventListener("click", function(event) {
     alert("Te estás moviendo al siguiente artista.");
+});
+
+}
+
 });
